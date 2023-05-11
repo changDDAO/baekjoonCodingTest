19b5beaad7ff09eb7466f03149c7d6576fee8c6d
Changhogod <younch8342@naver.com>
2023-05-12 00:26:45 +0900

    solve divisor problem

---
+++ src/main/java/basic/Beak1037.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Beak1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int N = Integer.parseInt(s);
+        s= br.readLine();
+        int[] numArr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt)
+                .sorted().toArray();
+        System.out.println(numArr[0]*numArr[numArr.length-1]);
+
+    }
+}
