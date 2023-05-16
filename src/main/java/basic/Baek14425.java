78a4acf7ab419489c1112486045a3d7a4f2ee7e3
Changhogod <younch8342@naver.com>
2023-05-17 00:17:50 +0900

    simple Problem use HashSet

---
+++ src/main/java/basic/Baek14425.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Baek14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            set.add(s);
+        }
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+            if(set.contains(s))cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
