61f8866a080632a3726086fadb575f1a7a3a7fe4
Changhogod <younch8342@naver.com>
2023-05-17 19:31:34 +0900

    simple Problem use HashMap

---
+++ src/main/java/basic/Baek10816.java
@@ -0,0 +1,33 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Baek10816 {
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int key = Integer.parseInt(st.nextToken());
+            Integer temp = map.get(key);
+            if (temp == null) {
+                sb.append(0 + " ");
+            } else {
+                sb.append(temp+" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
