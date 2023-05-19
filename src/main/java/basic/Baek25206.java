5202f32bc77e6b188001de0978f1bdab2ef70b8f
Changhogod <younch8342@naver.com>
2023-05-19 18:43:14 +0900

    ing~

---
+++ src/main/java/basic/Baek25206.java
@@ -0,0 +1,13 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek25206 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
