60b6b1dc5f81f3b4e86ac82fa65fb6c1d00509c8
Changhogod <younch8342@naver.com>
2023-05-28 15:46:57 +0900

    solve change BFS

--- src/main/java/basic/Baek16236.java
+++ src/main/java/basic/Baek16236.java
@@ -3,6 +3,7 @@ package basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Baek16236 {
     static int[][] nextDir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};//위 왼순으로 ㄱㄱ
@@ -23,11 +24,11 @@ public class Baek16236 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         srcMap = new int[N][N];
-        boolean[][] visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
+            String[] temp = s.split(" ");
             for (int j = 0; j < N; j++) {
-                    srcMap[i][j]=s.charAt(j)-'0';
+                    srcMap[i][j]=Integer.parseInt(temp[j]);
             }
         }
         //원본 맵초기화
@@ -37,9 +38,52 @@ public class Baek16236 {
             for (int j = 0; j < N; j++) {
                 if (srcMap[i][j] == 9) {
                     curPos = new int[]{i,j};
+                    srcMap[i][j]=0;
                 }
             }
         }
+
+        int fishSize = 2;//초기값
+        int eatQuantity = 0;//먹은 물고기양
+        int moveDis = 0; //움직인 거리
+
+        while (true) {
+            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) ->
+                    o1[2] != o2[2] ? Integer.compare(o1[2], o2[2]) : o1[0] != o2[0] ? Integer.compare(o1[0], o2[0])
+                            : Integer.compare(o1[1], o2[1]));
+            boolean visited[][] = new boolean[N][N];
+            pq.offer(new int[]{curPos[0], curPos[1], 0});
+            visited[curPos[0]][curPos[1]]=true;
+            boolean checkEat = false;
+            while (!pq.isEmpty()) {
+                curPos = pq.poll();
+                if (srcMap[curPos[0]][curPos[1]] != 0 && srcMap[curPos[0]][curPos[1]] < fishSize) {
+                    srcMap[curPos[0]][curPos[1]]=0;
+                    eatQuantity++;
+                    moveDis+=curPos[2];
+                    checkEat=true;
+                    break;
+                }
+                for (int[] next : nextDir) {
+                    int nx =curPos[0]+next[0];
+                    int ny = curPos[1]+next[1];
+                    if(nx<0||nx>=N||ny<0||ny>=N) continue;
+                    if(visited[nx][ny])continue;
+                    if(srcMap[nx][ny]>fishSize) continue;
+                    pq.offer(new int[]{nx, ny, curPos[2] + 1});
+                    visited[nx][ny]=true;
+                }
+            }
+            if (!checkEat) {
+                break; //전체반복종료
+            }
+            if (eatQuantity == fishSize) {
+                fishSize++;
+                eatQuantity=0;
+            }
+        }
+        System.out.println(moveDis);
+
 
 //https://girawhale.tistory.com/39
     }
