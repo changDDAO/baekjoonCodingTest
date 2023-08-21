20796931ae4f147b37b367ddf5233b1e70084f8a
Changhogod <younch8342@naver.com>
2023-08-21 21:37:00 +0900

    solve greedy problem

---
+++ src/main/java/gold/Baek13305.java
@@ -0,0 +1,34 @@
+package gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek13305 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dist = new long[n-1];
+        long[] cost = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            dist[i] = Long.parseLong(st.nextToken());
+        }
+        st= new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cost[i] = Long.parseLong(st.nextToken());
+        }
+        long minCost = cost[0];
+        long accSum  =0;
+        for (int i = 0; i < n-1; i++) {
+            if(cost[i]<minCost)
+                minCost = cost[i];
+            accSum+=minCost * dist[i];
+
+        }
+        System.out.println(accSum);
+
+    }
+}
--- src/main/java/gold/Baek1916.java
+++ src/main/java/gold/Baek1916.java
@@ -1,10 +1,9 @@
 package gold;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Baek1916 {
     static int n, m;
@@ -26,8 +25,28 @@ public class Baek1916 {
             return this.dist- o.dist;
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        for(int i=0; i<=n;i++)
+        graph.add(new ArrayList<>());
+        dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(a).add(new City(b,cost));
+            //각 노드간 비용 초기화 해주기
+        }
+        String[] temp = br.readLine().split(" ");
+        int start =  Integer.parseInt(temp[0]);
+        int end =  Integer.parseInt(temp[1]);
+        System.out.println(dijkstra(start, end));
 
     }
     static int dijkstra(int start, int end) {
@@ -39,9 +58,13 @@ public class Baek1916 {
             if (!visited[cur.cityNum]) {
                 visited[cur.cityNum]=true;
                 for (City city : graph.get(cur.cityNum)) {
-                    if(dist[city.cityNum]>dist[cur.cityNum]+city.dist)
+                    if (!visited[city.cityNum]&&dist[city.cityNum] > dist[cur.cityNum] + city.dist) {
+                        dist[city.cityNum]=dist[cur.cityNum]+city.dist;
+                        pq.offer(new City(city.cityNum, dist[city.cityNum]));
+                    }
                 }
             }
         }
+        return dist[end];
     }
 }
